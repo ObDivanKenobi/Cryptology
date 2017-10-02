a557c34d0bbf94b8d08c39940f4ec4249c3b4053
Ilia Manshin <ilia.manshin@yandex.ru>
2017-10-02 17:49:06 +0300

    Task1&Task2 complete

---
+++ Cryptology/Calculations.cs
@@ -0,0 +1,202 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Numerics;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Cryptology
+{
+    public static class Calculations
+    {
+        /// <summary>
+        /// Решение сравнения a^x = b (mod m) (перебор степеней a для нахождения x).
+        /// </summary>
+        public static int DiscreteLogarithmBruteforce(int a, int b, int m)
+        {
+            int x = 1;
+            BigInteger an = a;
+            while (an % m != b)
+            {
+                an *= a;
+                ++x;
+            }
+
+            return x;
+        }
+
+        /// <summary>
+        /// Решение сравнения a^x = b (mod m) (Алгоритм Шенкса).
+        /// </summary>
+        public static int DiscreteLogarithmShanksMethod(int a, int b, int m)
+        {
+            int n = (int)Math.Sqrt(m) + 1;
+            BigInteger an = 1;
+            for (int i = 0; i < n; ++i)
+                an = (an * a) % m;
+
+            var values = new Hashtable();
+            BigInteger current = an;
+            for (int i = 1; i <= n; ++i)
+            {
+                if (!values.ContainsKey(current))
+                    values[current] = i;
+
+                current = (current * an) % m;
+            }
+
+            current = b;
+            for (int i = 0; i <= n; ++i)
+            {
+                if (values.ContainsKey(current))
+                {
+                    BigInteger answer = (int)values[current] * n - i;
+                    if (answer < m)
+                        return (int)answer;
+                }
+
+                current = (current * a) % m;
+            }
+            return -1;
+        }
+
+        /// <summary>
+        /// Решение сравнения x^<paramref name="a"/> = <paramref name="b"/> (mod <paramref name="m"/>) (перебор значений x^<paramref name="a"/>).
+        /// </summary>
+        public static int PowerComparisonBruteforce(int a, int b, int m)
+        {
+            if (b == 0)
+                return m;
+            
+            int x = 1;
+            BigInteger xa = BigInteger.Pow(x, a);
+
+            while ( xa % m != b)
+            {
+                ++x;
+                xa = BigInteger.Pow(x, a);
+            }
+
+            return x;
+        }
+
+        /// <summary>
+        /// Проверка числа на простоту.
+        /// </summary>
+        public static bool IsPrime(int n)
+        {
+            if (n < 0)
+                throw new ArgumentException("Только натуральное число можно проверить на простоту");
+            if (n == 1)
+                return false;
+
+            int numberSqrt = (int)Math.Round(Math.Sqrt(n));
+            for (int i = 2; i <= numberSqrt; ++i)
+                if (n % i == 0)
+                    return false;
+
+            return true;
+        }
+
+        /// <summary>
+        /// Разложение числа на простые множители.
+        /// </summary>
+        public static Dictionary<int, int> Factorize(int n)
+        {
+            if (IsPrime(n))
+                return new Dictionary<int, int> { [n] = 1 };
+
+            var factors = new Dictionary<int, int>();
+            int numberSqrt = (int)Math.Round(Math.Sqrt(n));
+
+            for(int i = 2; i < numberSqrt; ++i)
+                while(n % i == 0)
+                {
+                    if (factors.ContainsKey(i))
+                        factors[i]++;
+                    else
+                        factors[i] = 1;
+
+                    n /= i;
+                }
+
+            if (n != 1)
+                factors[n] = 1;
+
+            return factors;
+        }
+
+        /// <summary>
+        /// Вычисление функции Эйлера.
+        /// </summary>
+        public static int EulersTotientFunction(int n)
+        {
+            var factors = Factorize(n);
+            int result = 1;
+
+            foreach (var p in factors)
+                result *= (int)Math.Pow(p.Key, p.Value - 1) * (p.Key - 1);
+
+            return result;
+        }
+
+        /// <summary>
+        /// Подсчёт количества примитивных элементов в поле вычетов по модулю m.
+        /// </summary>
+        public static int CountPrimitiveElements(int m)
+        {
+            return EulersTotientFunction(m - 1);
+        }
+
+        /// <summary>
+        /// Является ли <paramref name="n"/> примитивным элементом в поле вычетов по модулю <paramref name="m"/>.
+        /// </summary>
+        public static bool IsPrimitiveElement(int number, int m)
+        {
+            if (!IsPrime(m))
+                throw new ArgumentException("Модуль должен быть простым числом!");
+
+            int n = m - 1;
+            var factors = Factorize(n);
+            
+            foreach(var p in factors)
+            {
+                BigInteger modPow = BigInteger.ModPow(number, new BigInteger(n / p.Key), m);
+                if (modPow == BigInteger.One)
+                    return false;
+            }
+
+            return true;
+        }
+
+        /// <summary>
+        /// Поиск всех примитивных элементов в поле вычетов по модулю <paramref name="m"/>.
+        /// </summary>
+        public static List<int> FindPrimitiveElements(int m)
+        {
+            if (!IsPrime(m))
+                throw new ArgumentException("Модуль должен быть простым числом!");
+
+            var result = new List<int>();
+            int n = m - 1;
+            var factors = Factorize(n);
+
+            for (int number = 1; number <= n; ++number)
+            {
+                bool ok = true;
+                foreach (var p in factors)
+                {
+                    ok &= BigInteger.ModPow(number, new BigInteger(n / p.Key), m) != BigInteger.One;
+                    if (!ok)
+                        break;
+                }
+
+                if (ok)
+                    result.Add(number);
+            }
+
+            return result;
+        }
+    }
+}
---
+++ CryptologyTests/CalculationsTests.cs
@@ -0,0 +1,170 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Cryptology;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Diagnostics;
+
+namespace Cryptology.Tests
+{
+    [TestClass()]
+    public class CalculationsTests
+    {
+        [TestMethod()]
+        public void DiscreteLogarithmBruteforceTest()
+        {
+            //a^x = b mod m
+            (int a, int b, int m, int correctResult)[] input = new[] { (2, 23, 35, 7),
+                                                                       (54, 196, 596, 9),
+                                                                       (270, 342, 503, 49)};
+
+            foreach (var e in input)
+                if (Calculations.DiscreteLogarithmBruteforce(e.a, e.b, e.m) != e.correctResult)
+                    Assert.IsTrue(false);
+
+            Assert.IsTrue(true);
+        }
+
+        [TestMethod()]
+        public void DiscreteLogarithmShanksMethodTest()
+        {
+            //a^x = b mod m
+            (int a, int b, int m, int correctResult)[] input = new[] { (2, 23, 35, 7),
+                                                                       (54, 196, 596, 9),
+                                                                       (270, 342, 503, 49)};
+
+            foreach (var e in input)
+                if (Calculations.DiscreteLogarithmShanksMethod(e.a, e.b, e.m) != e.correctResult)
+                    Assert.IsTrue(false);
+
+            Assert.IsTrue(true);
+        }
+
+        [TestMethod()]
+        public void PowerComparisonBruteforceTest()
+        {
+            //x^a = b mod m
+            (int a, int b, int m, int correctResult)[] input = new[] { (7, 23, 35, 2),
+                                                                       (9, 196, 596, 54),
+                                                                       (49, 342, 503, 270)};
+
+            foreach (var e in input)
+                if (Calculations.PowerComparisonBruteforce(e.a, e.b, e.m) != e.correctResult)
+                    Assert.IsTrue(false);
+
+            Assert.IsTrue(true);
+        }
+
+        [TestMethod()]
+        public void IsPrimeTest()
+        {
+            (int input, bool correctResult)[] input = new[] { (1, false), (23, true),
+                                                               (1543476, false), (1964323, true)};
+
+            foreach (var e in input)
+                if (Calculations.IsPrime(e.input) != e.correctResult)
+                    Assert.IsTrue(false);
+
+            Assert.IsTrue(true);
+        }
+
+        [TestMethod()]
+        public void FactorizeTest()
+        {
+            int number = 111111111;
+            var result = Calculations.Factorize(number);
+
+            StringBuilder output = new StringBuilder($"{number} =");
+            foreach (var a in result)
+            {
+                output.Append($" {a.Key}");
+                if (a.Value != 1)
+                    output.Append($"^{a.Value}");
+            }
+
+            Debug.WriteLine(output.ToString());
+
+            Assert.IsTrue(true);
+        }
+
+        [TestMethod()]
+        public void EulersTotientFunctionTest()
+        {
+            (int input, int correctResult)[] input = new[] { (1, 1), (23, 22),
+                                                               (1543476, 467280), (61242, 19952)};
+
+            foreach (var e in input)
+                if (Calculations.EulersTotientFunction(e.input) != e.correctResult)
+                    Assert.IsTrue(false);
+
+            Assert.IsTrue(true);
+        }
+
+        [TestMethod()]
+        public void IsPrimitiveElementTest()
+        {
+            (int number, int module)[] input = new[] { (1, 2), (7, 29), (18, 29),
+                                                       (435, 787), (436, 787),
+                                                       (2822, 3853), (3675, 3853), (398983, 860941), (398991, 860941)};
+
+            foreach (var e in input)
+            {
+                var primitiveElements = Calculations.FindPrimitiveElements(e.module);
+                bool contains = primitiveElements.Contains(e.number),
+                     isPrimitive = Calculations.IsPrimitiveElement(e.number, e.module);
+
+                //bedug
+                /*
+                if (contains != isPrimitive)
+                {
+                    StringBuilder trace = new StringBuilder();
+                    trace.Append('-', 60);
+                    trace.AppendLine();
+                    foreach (var entry in primitiveElements)
+                        trace.Append($" {entry}");
+                    trace.AppendLine();
+                    trace.Append($"{(contains ? "contains" : "does no contain")} {e.number} but it {(isPrimitive ? "is" : "isn't")} primitive");
+
+                    Debug.WriteLine(trace);
+                }
+                */
+
+                Assert.AreEqual(contains, isPrimitive);
+            }
+        }
+
+        [TestMethod()]
+        public void FindPrimitiveElementsTest()
+        {
+            (int module, int correctQuantity)[] input = new[] { (2, Calculations.EulersTotientFunction(2-1)),
+                                                                (13, Calculations.EulersTotientFunction(13-1)),
+                                                                (29, Calculations.EulersTotientFunction(29-1)),
+                                                                (47, Calculations.EulersTotientFunction(47-1)),
+                                                                (787, Calculations.EulersTotientFunction(787-1)),
+                                                                (3853, Calculations.EulersTotientFunction(3853-1)),
+                                                                (9973, Calculations.EulersTotientFunction(9973-1)),
+                                                                (860941, Calculations.EulersTotientFunction(860941-1))};
+            //debug
+            //bool isOk = true;
+
+            foreach(var e in input)
+            {
+                StringBuilder output = new StringBuilder($"{e.module}:");
+                var foundElements = Calculations.FindPrimitiveElements(e.module);
+                bool isCurrentOk = foundElements.Count == e.correctQuantity;
+                //debug
+                //isOk &= isCurrentOk;
+
+                //foreach (var element in foundElements)
+                //    output.Append($" {element}");
+                //if (!isCurrentOk)
+                //    output.Append($" failed — quantity of found elements ({foundElements.Count}) != correct quantity ({e.correctQuantity}).");
+
+                Assert.IsTrue(isCurrentOk);
+                Debug.WriteLine(output.ToString());
+            }
+        }
+    }
+}
